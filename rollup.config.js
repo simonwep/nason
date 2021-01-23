8b11b4fe795cf64612c5fbf7c47be6f9ea575188
Simon R <simon@reinisch.io>
2021-01-23 19:13:35 +0100

    Add build:watch script
    Update LICENSE

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
 import {terser} from 'rollup-plugin-terser';
-import ts       from '@wessberg/rollup-plugin-ts';
-import replace  from '@rollup/plugin-replace';
-import pkg      from './package.json';
+import ts from '@wessberg/rollup-plugin-ts';
+import replace from '@rollup/plugin-replace';
+import pkg from './package.json';
 
 const banner = `/*! Nason ${pkg.version} MIT | https://github.com/Simonwep/nason */`;
+const production = process.env.NODE_ENV === 'production';
 
 export default {
     input: 'src/index.ts',
     plugins: [
         ts(),
-        terser(),
+        (production && terser()),
         replace({
             VERSION: JSON.stringify(pkg.version)
         })
